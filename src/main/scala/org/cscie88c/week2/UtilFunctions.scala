0b783e3bce1bdda9fd65415ebdadcb144e627893
TTAjiboye <112738974+TTAjiboye@users.noreply.github.com>
2022-09-10 22:35:30 -0400

    Update UtilFunctions.scala

--- src/main/scala/org/cscie88c/week2/UtilFunctions.scala
+++ src/main/scala/org/cscie88c/week2/UtilFunctions.scala
@@ -3,7 +3,9 @@ package org.cscie88c.week2
 object UtilFunctions {
 
   // complete the functions below
-  def maximum(a: Int, b: Int): Int = ???
+  def maximum(a: Int, b: Int): Int = {
+    if (a > b) a else b
+  }
   def average(a: Int, b: Int): Double = ???
 
 }
